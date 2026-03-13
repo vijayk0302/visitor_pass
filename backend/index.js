8fe506b9d73f8b8afd1b5440064b299d045f135b
vijayk0302 <vijay740412@gmail.com>
2026-03-13 21:16:24 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,23 @@
-import app from './src/app.js'
-import dotenv from 'dotenv'
+import app from "./src/app.js";
+import dotenv from "dotenv";
 dotenv.config();
 
-import dbconnect from './src/config/db.js';
+import dbconnect from "./src/config/db.js";
 
-dbconnect()
+dbconnect();
 
+const PORT = process.env.PORT || 3000;
 
-const PORT=process.env.PORT || 3000
+const startServer = async () => {
+  try {
+    await dbconnect();
 
-app.listen(PORT,()=>{
-    console.log(`server is up and running on http://localhost:${PORT} `);
-    
-})
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error("Failed to start server:", error);
+  }
+};
+
+startServer();
